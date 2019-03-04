539fe2daeab6e1aebf5f211480d8c4b3ae92b508
Mugacha Gichamba <mugachadjango@gmail.com>
2019-03-04 15:31:32 +0300

    added function to initialise player1 and player2 variables with names, score and which player is currently active

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,23 @@ function player(name, turn, total) {
     this.turn = turn;
     this.total = total;
 }
+
+var player1, player2;
+
+$(document).ready(function() {
+    $("form#new-players").submit(function(event) {
+        event.preventDefault();
+        player1 = new player($("input#new-player1-name").val(), true, 0);
+        player2 = new player($("input#new-player2-name").val(), false, 0);
+        $("div#player_names").hide();
+
+
+        $("#column_1 h1#player1 ").text(player1.name);
+        $("#column_3 h1#player2 ").text(player2.name);
+        $("div.col-md-4").show();
+        $("h1#whose_turn ").text("its " + player1.name + "'s turn");
+        $("button#roll").click(rollDice);
+        $("button#hold").click(hold);
+
+    })
+});

a2b5855b928cfb8f7a7a35f8a6eb73587b9eea97
Mugacha Gichamba <mugachadjango@gmail.com>
2019-03-05 09:45:56 +0300

    Finished on final indentation and spacing

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,6 @@ $(document).ready(function() {
         player1 = new player($("input#new-player1-name").val(), true, 0);
         player2 = new player($("input#new-player2-name").val(), false, 0);
         $("div#player_names").hide();
-
-
         $("#column_1 h1#player1 ").text(player1.name);
         $("#column_3 h1#player2 ").text(player2.name);
         $("div.col-md-4").show();
@@ -28,7 +26,6 @@ $(document).ready(function() {
 var current = 0;
 
 function rollDice() {
-
 
     rolledValue = Math.floor(Math.random() * (7 - 1) + 1);
     console.log(rolledValue);
